fb300a881a8ce900fc99148e83d65d8a16ec5477
JermixGames <jermixcastillo@gmail.com>
2022-12-09 16:33:47 -0500

    Sistema de combate terminado

--- Jeremy's gacha haven/elGacha.cpp
+++ Jeremy's gacha haven/elGacha.cpp
@@ -144,6 +144,8 @@ struct PJ
     bool party = false;
 
 
+
+
     void SetStats(string n, string e, int a, int b, int c)
     {
         name = n;
@@ -154,6 +156,13 @@ struct PJ
         lvl = 0;
 
     }
+    int cpower()
+    {
+        int power;
+        power = ATK + HP;
+        return power;
+
+    }
 };
 struct Mob
 {
@@ -162,6 +171,7 @@ struct Mob
     int HP;
     int ATK;
     int lvl;
+   
     void SetStats(string n, string e, int a, int b)
     {
         name = n;
@@ -170,6 +180,12 @@ struct Mob
         ATK = b;
         lvl = 0;
 
+    }
+    int cpower()
+    {
+        int power;
+        power = ATK + HP;
+        return power;
     }
 };
 
@@ -208,19 +224,20 @@ PJ PJmenu(PJ C,int & PartyC)
 
         system("cls");
         cout << C.name << endl;
+        cout << "You have " << crystals << " crystals!\n" << endl;
 
         if (C.party == false)
         {
 
             cout << "0) Meter a la Party.\n";
-            cout << "1) Entrenar personaje.\n";
+            cout << "1) Entrenar personaje|1000 Crystals.\n";
             cout << "2) Ver estadisticas.\n";
             cout << "3) salir.\n";
         }
         if (C.party == true)
         {
             cout << "0) Sacar de la Party.\n";
-            cout << "1) Entrenar personaje.\n";
+            cout << "1) Entrenar personaje|1000 Crystals.\n";
             cout << "2) Ver estadisticas.\n";
             cout << "3) salir.\n";
         }
@@ -260,6 +277,7 @@ PJ PJmenu(PJ C,int & PartyC)
             if (crystals >= 1000)
             {
                 system("cls");
+                cout << "You have " << crystals << " crystals!\n\n" << endl;
 
                 cout << C.name << " SUBE DE NIVEL!!!!\n";
                 cout << "|Nivel" << C.lvl << "|ATK" << C.ATK << "|HP" << C.HP << "|\n";
@@ -275,6 +293,7 @@ PJ PJmenu(PJ C,int & PartyC)
             else
             {
                 system("cls");
+                cout << "You have " << crystals << " crystals!\n\n" << endl;
 
                 cout << " no hay plata \n";
                 system("pause");
@@ -330,6 +349,8 @@ int DMenu( int M )
   \____/ \___|_|  \___|_| |_| |_|\__, | |___/  \_____|\__,_|\___|_| |_|\__,_| |_| |_|\__,_| \_/ \___|_| |_|
                                   __/ |                                                                    
                                  |___/                                                                     	
+
+
 )" << '\n';
 
 
@@ -338,7 +359,7 @@ int DMenu( int M )
     cout << "1) Roll Character for 500 crystals?\n";// funcionando pero con trabajo por hacer
     cout << "2) Guarantee a 5 star character by rolling for 1500 crystals.\n";// en proceso 
     cout << "3) Train character for 1000 crystals.\n";//descartado es movido a la seccion  character
-    cout << "4) Battle the next enemy (Name: XXXX, Element: XXXX, Level: XXXX)\n";//en proceso 
+    cout << "4) adentrase en la batalla\n";//en proceso 
     cout << "5) Play Alto-Bajo to farm crystals.\n";//terminado(mejoras visuales pendientes
     cout << "6) Exit the game.\n";//es lo que mejor funciona 
     cin >> M;
@@ -353,6 +374,7 @@ int main()
     int Party = 0;
     int pmenu;
     int use;
+    int sumaPW;
 
     srand((unsigned)time(0));
 
@@ -364,13 +386,15 @@ int main()
     PJ personaje[7];
     personaje[0].SetStats("Nahida", "Dendro", 500, 250, 6);
     personaje[1].SetStats("Ember", "Pyro", 500, 250, 6);
-    personaje[2].SetStats("Gaia", "Dendro", 150, 100, 5);
-    personaje[3].SetStats("Blaze", "Pyro", 150, 100, 5);
-    personaje[4].SetStats("Haru", "Dendro", 150, 100, 5);
-    personaje[5].SetStats("Hestia", "Pyro", 150, 100, 5);
+    personaje[2].SetStats("Gaia", "Dendro", 250, 100, 5);
+    personaje[3].SetStats("Blaze", "Pyro", 250, 100, 5);
+    personaje[4].SetStats("Haru", "Dendro", 250, 100, 5);
+    personaje[5].SetStats("Hestia", "Pyro", 250, 100, 5);
 
     PJ mipj;
-    
+    Mob enemie;
+    enemie.SetStats("enemigo", "Neutro", 700,200);
+
 
  
 
@@ -455,6 +479,68 @@ int main()
             break;
 
         case 4:
+            //Sistema de batalla
+            do
+            {
+
+            
+                system("cls");
+                cout << "BOSQUE DE BATALLA\n";
+                cout << "PARTY\n";
+                 sumaPW = 0;
+                for (int p = 0; p <= 5; p++)
+                {
+                
+                    if (personaje[p].party == true)
+                    {
+                        cout  << "|" << personaje[p].name;
+                        sumaPW = sumaPW + personaje[p].cpower();
+                    }
+                
+
+                }
+                cout << "|\n|POWER|"<< sumaPW <<"\n";
+                cout << "-------------------------------\n";
+
+                cout << "|ENEMIGO|\n";
+                cout << "-------------------------------\n";
+                cout << "|" << enemie.name << "||" << enemie.element << "|POWER|" << enemie.cpower() << "|\n";
+
+                cout << "-------------------------------\n\n\n";
+                cout << "1| LUCHAR\n";
+                cout << "2| retirarse\n";
+                cin >> pmenu;
+                if (pmenu == 1)
+                {
+                    //pelea
+                    system("cls");
+                    if (sumaPW >= enemie.cpower())
+                    {
+                        cout << "ENEMIGO DERROTADO!!!!\n";
+                        cout << "HAS GANADO 250 Crytals\n";
+                        crystals += 250;
+                        cout << "EL siguiente enemigo es mas poderoso, CUIDADO!\n";
+                        enemie.ATK = enemie.ATK * 2;
+                        enemie.HP = enemie.HP * 2;
+                        system("pause");
+                        pmenu = 0;
+                    }
+                    if (sumaPW < enemie.cpower() && pmenu == 1) 
+                    {
+
+
+                        cout << "AUN NO PUEDES CON EL!!!\n";
+                        cout << "Entrena a tus personajes o consigue personajes mas fuertes en el gacha\n";
+                        cout << "Pierdes 250 Crystals\n";
+                        crystals -= 250;
+
+                    }
+
+
+                    system("pause");
+                }
+
+            } while (pmenu !=2);
 
             break;
 
@@ -473,6 +559,8 @@ int main()
   | |_| | | / ___ | | | |  _ (|___ |    | | | |_| | |      | |_| | |/ ___ | |_| | | | | ( (_| |
    \__)_| |_\_____|_| |_|_| \_|___/     |_|  \___/|_|      |  __/ \_)_____|\__  |_|_| |_|\___ |
                                                            |_|            (____/        (_____|
+
+
 )" << '\n';
 
 
